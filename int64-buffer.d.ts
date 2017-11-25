22baa88b200b77b0e9c48f74e673752f655cba95
Yusuke Kawasaki <u-suke@kawa.net>
2017-11-25 09:06:28 +0900

    spaces
    thanks to WebStorm

--- int64-buffer.d.ts
+++ int64-buffer.d.ts
@@ -2,7 +2,7 @@
 
 type ArrayType = Uint8Array | ArrayBuffer | number[];
 
-declare abstract class Int64 
+declare abstract class Int64
 {
 	constructor(value?: number);
 	constructor(high: number, low: number);
@@ -17,24 +17,33 @@ declare abstract class Int64
 	constructor(array: ArrayType, offset: number, value: string, radix?: number);
 
 	toNumber(): number;
+
 	toJSON(): number;
+
 	toString(radix?: number): string;
+
 	toBuffer(raw?: boolean): Buffer;
+
 	toArrayBuffer(raw?: boolean): ArrayBuffer;
+
 	toArray(raw?: boolean): number[];
 }
+
 export declare class Int64BE extends Int64
 {
 	static isInt64BE(obj: any): obj is Int64BE;
 }
+
 export declare class Uint64BE extends Int64
 {
 	static isUint64BE(obj: any): obj is Uint64BE;
 }
+
 export declare class Int64LE extends Int64
 {
 	static isInt64LE(obj: any): obj is Int64LE;
 }
+
 export declare class Uint64LE extends Int64
 {
 	static isUint64LE(obj: any): obj is Uint64LE;
